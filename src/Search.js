bc65cdc191dcf06a64f20ecc65bf8ab3a506d184
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-06-15 14:10:39 -0700

    search form structure

--- src/Search.js
+++ src/Search.js
@@ -3,7 +3,28 @@ import React, { Component } from 'react';
 class Search extends Component {
   render () {
     return (
-      <h1>Search</h1>
+      <div className='panel panel-default'>
+        <div className='panel-heading'>
+          <h3 className='panel-title'>Search</h3>
+        </div>
+        <div className='panel-body'>
+          <form>
+            <div className='form-group'>
+              <label for='topic'>Topic</label>
+              <input type='text' className='form-control' id='topic' placeholder='Topic' required />
+            </div>
+            <div className='form-group'>
+              <label for='start-year'>Start Year</label>
+              <input type='text' className='form-control' id='start-year' placeholder='Start Year' />
+            </div>
+            <div className='form-group'>
+              <label for='end-year'>End Year</label>
+              <input type='text' className='form-control' id='end-year' placeholder='End Year' />
+            </div>
+            <button type='submit' className='btn btn-default'>Search</button>
+          </form>
+        </div>
+      </div>
     );
   }
 }
